f4c74576d52331e93850e03096f2118401d4caa5
Richard Kotze <rkotze@dctfh.com>
2017-11-09 23:39:27 +0000

    build query for open cards on a board.

--- src/buildQuery.js
+++ src/buildQuery.js
@@ -1,7 +1,38 @@
-module.exports.baseQuery = function(query){
-    return Object.assign({
+const joinable = require('joinable').joinStrings;
+const BASE_URI = 'https://api.trello.com/1';
+const { TRELLO_API_KEY, TRELLO_API_TOKEN, TEST } = process.env;
+
+function baseQuery(query, authObject){
+    let newQuery = Object.assign({
         method: 'GET',
-        url: 'https://api.trello.com/1/',
+        url: BASE_URI,
         qs: {}
     }, query);
-}
+
+    let keyToken = authObject;
+    if(!TEST){
+        keyToken = {
+            key: TRELLO_API_KEY,
+            token: TRELLO_API_TOKEN
+        };
+    }
+
+    newQuery.qs = Object.assign(newQuery.qs, keyToken);
+    return newQuery;
+};
+
+function openCardsOnBoard(query, authObject){
+    let newQuery = {
+        url: joinable(BASE_URI, 'boards', query.idBoard, 'cards', 'open', { separator: '/' }),
+        qs: {
+            fields: query.fields,
+            members: true
+        }
+    };
+    return baseQuery(newQuery, authObject);
+};
+
+module.exports = {
+    baseQuery,
+    openCardsOnBoard
+};
--- src/buildQuery.spec.js
+++ src/buildQuery.spec.js
@@ -1,12 +1,45 @@
-const { baseQuery } = require('./buildQuery');
+const { baseQuery, openCardsOnBoard } = require('./buildQuery');
 
-describe('Building base queries', () => {
+describe('Building queries', () => {
     it('base query with trello api and method', () => {
-        const actual = baseQuery({ method: 'GET' });
+        const actual = baseQuery();
         expect(actual).toEqual({
             method: 'GET',
-            url: 'https://api.trello.com/1/',
-            qs: {}
+            url: 'https://api.trello.com/1',
+            qs: {
+
+            }
+        });
+    });
+
+    it('base query send in new key and tokens', () => {
+        const actual = baseQuery({}, { key: '12', token: '45' });
+        expect(actual).toEqual({
+            method: 'GET',
+            url: 'https://api.trello.com/1',
+            qs: {
+                key: '12', 
+                token: '45' 
+            }
+        });
+    });
+    
+    it('open cards on a board', () => {
+        const actual = openCardsOnBoard({
+            idBoard: 'ysekw',
+            fields: 'id,name,shortUrl,url,idList'
+        }, 
+            { key: '12', token: '45' });
+
+        expect(actual).toEqual({
+            method: 'GET',
+            url: 'https://api.trello.com/1/boards/ysekw/cards/open',
+            qs: {
+              fields: 'id,name,shortUrl,url,idList',
+              members: true,
+              key: '12',
+              token: '45'
+            }
         });
     });
 });
