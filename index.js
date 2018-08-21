15f97c2ff324a0133ae529a9fad4f8ac14adbd38
Richard Kotze <rkotze@findmypast.com>
2018-08-21 19:15:32 +0100

    Use node to open editor for commit message

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
+const { spawn } = require('child_process');
 
 const { chTrello } = require('./src/terminalThemes');
 const { getUserBoardCards } = require('./src/getCards');
@@ -16,6 +17,18 @@ program
   .description('Find your codes in a board')
   .option('-f, --listfilter <name>', 'Filter by list name')
   .action(getUserBoardCards);
+
+program
+  .command('commit')
+  .description('Find your codes in a board')
+  .action(function(){
+    const child = spawn('git', ['commit', '-m', `Trello ticket commit`, '-e'], {
+      stdio: 'inherit'
+    });
+    child.on('close', function (e, code) {
+      console.log("finished", e, code);
+    });
+  });
 
 program.parse(process.argv);
 
